0b6d8af19fd42d920d80adbeafc4fbfcc9237af4
Yauheni Marchanka <v-ymarchanka@ebsco.com>
2020-07-23 00:48:28 -0400

    fix

--- build-and-run.sh
+++ build-and-run.sh
@@ -9,16 +9,14 @@ fi
 
 npm install
 
-docker build -t web-app . --no-cache
-
 #allow local images to be used by minikube
+eval $(minikube docker-env)
 docker stop registry
 docker rm registry
+docker build -t web-app .
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 docker tag web-app localhost:5000/web-app
 docker push localhost:5000/web-app
-
 kubectl apply -f k8s/resources.yaml
-
 URL=$(minikube service -n application web-app --url)
 echo "Open ${URL} in your browser"
